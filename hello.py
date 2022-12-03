9cbf78ad0280787e9b48ae547d81405e2f8008f5
paotg123 <91358256+paotg123@users.noreply.github.com>
2022-12-03 07:46:06 -0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,63 @@
+from cloudant import Cloudant
+from flask import Flask, render_template, request, jsonify
+import atexit
+import os
+import json
+
+import sys
+import random
+
+app = Flask(__name__, static_url_path='')
+
+db_name = 'mydb'
+client = None
+db = None
+
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.getenv('VCAP_SERVICES'))
+    print('Found VCAP_SERVICES')
+    if 'cloudantNoSQLDB' in vcap:
+        creds = vcap['cloudantNoSQLDB'][0]['credentials']
+        user = creds['username']
+        password = creds['password']
+        url = 'https://' + creds['host']
+        client = Cloudant(user, password, url=url, connect=True)
+        db = client.create_database(db_name, throw_on_exists=False)
+elif "CLOUDANT_URL" in os.environ:
+    client = Cloudant(os.environ['CLOUDANT_USERNAME'], os.environ['CLOUDANT_PASSWORD'], url=os.environ['CLOUDANT_URL'], connect=True)
+    db = client.create_database(db_name, throw_on_exists=False)
+elif os.path.isfile('vcap-local.json'):
+    with open('vcap-local.json') as f:
+        vcap = json.load(f)
+        print('Found local VCAP_SERVICES')
+        creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
+        user = creds['username']
+        password = creds['password']
+        url = 'https://' + creds['host']
+        client = Cloudant(user, password, url=url, connect=True)
+        db = client.create_database(db_name, throw_on_exists=False)
+
+# On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
+# When running this app on the local machine, default the port to 8000
+port = int(os.getenv('PORT', 8000))
+
+@app.route('/')
+def root():
+    puntos = []
+    for i in range(10):
+        puntos.append({"id": i, "x": random.uniform(0,10), "y": random.uniform(0,10), "z": random.uniform(0,10)})
+        
+        #Como hacer print
+        print("asi se imprime", file=sys.stdout)
+       
+       #como sacar valores random
+    return {"carros": puntos} 
+
+
+@atexit.register
+def shutdown():
+    if client:
+        client.disconnect()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, debug=True)
